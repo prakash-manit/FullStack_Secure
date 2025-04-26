2f0b52d7377b68b0261eadf78c94a9cbeafcc2e7
Prakash <prakash.manit@gmail.com>
2025-04-26 12:25:48 +0530

    Fixed the Navbar and layout

--- Client/src/app/pages/home/home.component.ts
+++ Client/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
