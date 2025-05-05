c7cde21abdb0ff8fc83c9042d37974d64274ae97
Prakash <prakash.manit@gmail.com>
2025-05-05 22:48:25 +0530

    Added new functionality in register page

--- Client/src/app/pages/register/register.component.ts
+++ Client/src/app/pages/register/register.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select'
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIconModule, MatSelectModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  fb= inject(FormBuilder);
+  registerForm!: FormGroup;
+  router = inject(Router);
+  confirmPasswordHide: boolean = true;
+  passwordHide: boolean = true;
 
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required]],
+      fullName: ['',[Validators.required]],
+      roles: [''],
+      confirmPassword: ['',[Validators.required]],
+    });
+  }
 }
